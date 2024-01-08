80f41e52b42b06447f60194da83d648b56cf3c88
Jai18kishore <113165460+Jai18kishore@users.noreply.github.com>
2024-01-08 12:12:50 +0530

    commit

--- TANSAM/TANSAM WEBSITE/app.py
+++ TANSAM/TANSAM WEBSITE/app.py
@@ -6,11 +6,22 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+
+#HOME PAGE
 @app.route("/blogsDetails")
 def blogsDetails():
     return render_template("blogsdetails.html")
 
 
+
+#OUR SOLUTIONS
+@app.route("/reverseEngineering")
+def reverseEngineering():
+    return render_template("reverseengineering.html")
+
+
+#LABS
 @app.route("/productInnovation")
 def productInnovation():
     return render_template("pdinnov.html")
