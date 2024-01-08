4f53f2e730d80927e7af6ba9588ee1b00cdae5c2
Jayaram Aditthya <113106842+jayaditthya@users.noreply.github.com>
2024-01-08 16:04:55 +0530

    commit

--- TANSAM/TANSAM WEBSITE/app.py
+++ TANSAM/TANSAM WEBSITE/app.py
@@ -34,5 +34,11 @@ def innovationManufacturing():
 def ar_vr():
     return render_template("arvr.html")    
 
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True) 
