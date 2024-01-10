c01627cfe0e402f8321670f9f507f231b18955c5
Aswin283 <111261670+Aswin283@users.noreply.github.com>
2024-01-10 14:29:08 +0530

    commit

--- TANSAM/TANSAM WEBSITE/app.py
+++ TANSAM/TANSAM WEBSITE/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect,url_for,session
 
 app = Flask(__name__)
 
@@ -7,12 +7,10 @@ def index():
     return render_template("index.html")
 
 
-
 #HOME PAGE
 @app.route("/blogsDetails")
 def blogsDetails():
     return render_template("blogsdetails.html")
-
 
 
 #OUR SOLUTIONS
@@ -26,9 +24,11 @@ def reverseEngineering():
 def productInnovation():
     return render_template("pdinnov.html")
 
+
 @app.route("/innovationManufacturing")
 def innovationManufacturing():
     return render_template("innovmanu.html")  
+
 
 @app.route("/ar_vr")
 def ar_vr():
@@ -39,6 +39,16 @@ def ar_vr():
 def admin():
     return render_template("admin.html")
 
+@app.route("/adminLogin",methods=['POST','GET'])
+def adminLogin():
+    if request.method == 'POST':
+        username=request.form.get('username')
+        password=request.form.get('password')
+        print(username,password)
+        if request.form.get('clicked')=='Login Now':
+            return render_template("admin.html")
+    return render_template("index.html")
+        
 
 if __name__ == "__main__":
     app.run(debug=True) 
