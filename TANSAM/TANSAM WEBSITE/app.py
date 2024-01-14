f429490691b456d1fc798e140ada3a09ef332d36
Aswin283 <111261670+Aswin283@users.noreply.github.com>
2024-01-14 12:40:39 +0530

    commit

--- TANSAM/TANSAM WEBSITE/app.py
+++ TANSAM/TANSAM WEBSITE/app.py
@@ -1,10 +1,10 @@
 from flask import Flask,render_template,request,redirect,url_for,session
-
+import os
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("admin.html")
 
 
 #HOME PAGE
@@ -49,13 +49,43 @@ def adminIndex():
         password=request.form.get('password')
         print(username,password)
         if username=='admin' and password=='admin':
-            return render_template("admin.html")
+            return redirect(url_for('admin'))
     return render_template("index.html")
+
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
+@app.route("/adminPosts/<Type>")
+def adminPosts(Type):
+    return render_template("adminblogs.html",type=Type)        
+
+
+@app.route('/postDetails/<Type>', methods=['POST'])
+def postDetails(Type):
+    print(Type)
+    if request.method=='POST':
+        print(Type)
+        title=request.form.get('blogstitle')
+        date=request.form.get('blogsdate')
+        author=request.form.get('blogsauthor')
+        image=request.files['blogsimage']
+        #save image in static
+        image.save('static')
+        content=request.form.get('blogscontent')
+        print(title,date,author,content)
+        #create a notepad with name of the titile and write all the content in it
+        #save the notepad in static
+        fp=open('/static/'+title+'.txt','w')
+        fp.write(date+"\n\n")
+        fp.write(author+"\n\n")
+        fp.write(content)
+        fp.close()
         
-@app.route("/adminBlogs")
-
-
-
+        
+    return render_template("adminblogs.html")
 
 if __name__ == "__main__":
     try:
